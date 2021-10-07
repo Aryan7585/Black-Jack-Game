01e3f187390e43e0ed7e0ed8712dbcbf78691ce4
Aryan <89633258+ryan7585@users.noreply.github.com>
2021-10-07 16:27:39 +0530

    Add files via upload

---
+++ design.py
@@ -0,0 +1,18 @@
+from tkinter import LEFT,Y
+from tkinter import * 
+
+Table_font = "Courier New"
+
+bg_color = '#006600'
+
+font_small = (Table_font,24)
+font_medium = ( Table_font,36) 
+font_large = (Table_font,60)
+
+button_args = {'fg':'#FFFFFF','font' : font_small}
+
+pack_left_and_fill_y = {'side' : LEFT,'fill' : Y}
+
+highlight_frame_with_white = {'highlightthickness' : 1,'highlightcolor' : '#FFFFFF', 'highlightbackground' : '#FFFFFF'}
+
+
